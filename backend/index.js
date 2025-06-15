e79786c49cdafc99f297a62b4cd960ad4dd5ae65
ZeroOneCodes <tanujchaganti003@gmail.com>
2025-06-15 21:31:45 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,15 @@ const WorkingRoute = require("./working/routes");
 const AuthRoute = require("./auth/routes");
 const startServer = require("./checkDb");
 const express = require("express");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 const app = express();
+
+app.use(cors({
+  origin: process.env.FRONTEND_BASE_URL || "http://localhost:5173",
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(cookieParser());
