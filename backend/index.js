5c9116d7048b04d81956b901962c8df49f660be3
ZeroOneCodes <tanujchaganti003@gmail.com>
2025-06-29 16:45:10 +0530

    formated index.js

--- backend/index.js
+++ backend/index.js
@@ -5,35 +5,39 @@ const PostRoute = require("./posts/routes");
 const startServer = require("./checkDb");
 const express = require("express");
 const passport = require("passport");
-const { startWorker } = require('./queues/worker');
+const { startWorker } = require("./queues/worker");
 const cors = require("cors");
-const session = require('express-session');
+const session = require("express-session");
 const cookieParser = require("cookie-parser");
 require("./auth/passport");
 const app = express();
 
-app.use(cors({
-  origin: process.env.FRONTEND_BASE_URL || "http://localhost:5173",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_BASE_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(session({
-  secret: 'your_secret_key_here',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: false,
-    httpOnly: true,
-    maxAge: 1000 * 60 * 60 * 24, // 1 day
-  }
-}));
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(
+  session({
+    secret: "your_secret_key_here",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 startServer();
-startWorker()
+startWorker();
 app.use("/api/v1", AuthRoute);
 app.use("/api/v1", WorkingRoute);
 app.use("/api/v1", PostRoute);
