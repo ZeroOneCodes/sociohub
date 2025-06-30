b76ca971fe932616b03837b44850d80eb864b387
ZeroOneCodes <tanujchaganti003@gmail.com>
2025-06-30 22:08:52 +0530

    rename working controller

--- backend/working/routes.js
+++ backend/working/routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const WorkingController = require("./controler");
+const WorkingController = require("./controller");
 const { verifyAccessToken } = require("../auth/middleware");
 
 router.get("/working", verifyAccessToken, WorkingController.getWorking);
